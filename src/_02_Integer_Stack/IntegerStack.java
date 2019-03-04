3f217a19e2b6e9a873f025a0416dac75870ff018
ryder <ryder@MSI.sduhsd.lan>
2019-03-04 14:42:09 -0800

    Working on hashmaps got integer stack down

--- src/_02_Integer_Stack/IntegerStack.java
+++ src/_02_Integer_Stack/IntegerStack.java
@@ -2,49 +2,57 @@ package _02_Integer_Stack;
 
 public class IntegerStack {
 	//1. create a private array of integers
-	
+	private int[] arr;
 	//2. complete the constructor by initializing the member array
 	//   to a new array of length 0. This prevents null pointer exceptions.
 	public IntegerStack() {
+		arr = new int[0];
 	}
 	
 	//3. Complete the steps in the push method.
 	public void push(int v) {
+		
+			
 		//A. create a new array that is one element larger than the member array
-		
+		int[] arr2 = new int[arr.length+1];
 		//B. set the last element of the new array equal to the integer passed into the method
-		
+		arr2[arr2.length] = v;
 		//C. iterate through the member array and copy all the elements into the new array
-		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = arr2[i];
 		//D. set the member array equal to the new array.
-		
+			arr = arr2;
+		}
 	}
 	
 	//4. Complete the steps in the pop method.
 	public int pop() {
 		//A. create an integer variable and initialize it to the
 		//   last element of the member array.
-		
+		int v = arr[arr.length];
 		//B. create a new array that is one element smaller than the member array
-		
+		int[] arr3 = new int[arr.length-1];
 		//C. iterate through the new array and copy every element from the
 		//   member array to the new array
-		
+		for (int i = 0; i < arr3.length; i++) {
+			arr[i] = arr3[i];
+		}
 		//D. set the member array equal to the new array
-		
+		arr = arr3;
 		//E. return the variable you created in step A
-		return 0;
+		return v;
 	}
 	
 	//5. Complete the clear method to set the
 	//   member array to a new array of length 0
 	public void clear() {
-		
+		int[] arr4 = new int[0];
+		arr = arr4;
 	}
 	
 	//6. Complete the size method to return 
 	//   the length of the member array
 	public int size() {
-		return 0;
+		return arr.length;
 	}
 }
--- src/_06_Intro_To_Hash_Maps/_01_IntroToHashMaps.java
+++ src/_06_Intro_To_Hash_Maps/_01_IntroToHashMaps.java
@@ -5,16 +5,24 @@ import java.util.HashMap;
 public class _01_IntroToHashMaps {
 	public static void main(String[] args) {
 		//1. Create a HashMap called roots with Integers for the keys and Doubles for the values.
-
+		HashMap<Integer, Double> hsh = new HashMap<Integer, Double>();
 		//2. Using a for-loop, add 500 entries to your HashMap. The key entry will be the 
 		//   current iteration of the loop (i). The value entry will be the square root of i.
-
+		for (int i = 0; i < 500; i++) {
+			hsh.put(i, Math.sqrt(i));
+		}
 		//3. Iterate through all the entries in your HashMap displaying the keys with their respective
 		//   square roots (values). Use the following format.
-
+		for (int i = 0; i < 500; i++) {
+			
 		//   The square Root of 0 is 0.0
+			System.out.println(hsh.get(0));
 		//   The square Root of 1 is 1.0
+			System.out.println(hsh.get(1));
 		//   The square Root of 2 is 1.4142135623730951
+			System.out.println(hsh.get(3));
 		//   The square Root of 3 is 1.7320508075688772
+			System.out.println(hsh.get(4));
+		}
 	}
 }
