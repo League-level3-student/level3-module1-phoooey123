11bac26cc3bd049535141338576f0d626644812c
loganitis899 <league@wts20.attlocal.net>
2019-04-23 16:59:19 -0700

    :neutral_face:

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,55 @@
 package _04_HangMan;
 
-public class HangMan{
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class HangMan implements KeyListener{
+	JFrame f;
+	JPanel p;
+	JLabel l;
+	String word;
+	public static void main(String[] args) {
+		
+		
+			
+		new HangMan().start();
+	}
 	
-	
+	void start() {
+		Utilities u = new Utilities();
+		String words = JOptionPane.showInputDialog("How mahy words would you like to guess?");
+		word = u.readRandomLineFromFile("dictionary.txt");
+		f = new JFrame();
+		p = new JPanel();
+		l = new JLabel();
+		f.add(p);
+		p.add(l);
+		f.addKeyListener(this);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		char c = e.getKeyChar();
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
+ 
