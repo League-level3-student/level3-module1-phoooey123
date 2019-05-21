1b6c1dcef6b67d68dcd9a7dd377657b1929d6994
league <league@imac-93.attlocal.net>
2019-05-21 16:59:06 -0700

    Finished Hangman :sunglasses:

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -2,6 +2,7 @@ package _04_HangMan;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -15,7 +16,12 @@ public class HangMan implements KeyListener{
 	JLabel l1;
 	String word;
 	String display = "";
+	String str1 = "";
+	String str2 = "";
+	String current = "";
 	int cur = 0;
+	int lives = 10;
+	ArrayList <Character> used = new ArrayList <Character>();
 	Stack<String> Wstack = new Stack<String>();
 	public static void main(String[] args) {
 		
@@ -40,7 +46,8 @@ public class HangMan implements KeyListener{
 		f.setVisible(true);
 		f.setSize(200, 50);
 		f.addKeyListener(this);
-		String current = Wstack.pop();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		current = Wstack.pop();
 		cur = current.length();
 		
 		for (int i = 0; i < cur; i++) {
@@ -48,13 +55,39 @@ public class HangMan implements KeyListener{
 			
 			
 		}
-		l1.setText(display + "  " + "Lives Remaining: ");
+		l1.setText(display + "  " + "Lives Remaining: " + lives);
+		
+		
+	}
+		void finish() {
+		if(current.equals(display) && !Wstack.isEmpty()) {
+			current = Wstack.pop();
+			cur = current.length();
+			display = "";
+			
+			for (int i = 0; i < cur; i++) {
+				display+="_";
+				
+			}
+			lives = 10;
+			l1.setText(display + "  " + "Lives Remaining: " + lives);
+		}
+		else if (lives == 1) {
+			System.exit(0);
+		}
+		else if (current.equals(display) && Wstack.isEmpty()) {
+			JOptionPane.showMessageDialog(null, "You Win");
+			System.exit(0);
+		}
+		 
+		
+		
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		char c = e.getKeyChar();
+		
 		
 		
 	}
@@ -62,15 +95,27 @@ public class HangMan implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		for (int i = 0; i < cur; i++) {
-			
-		}
+		finish();
 	}
-
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		char c = e.getKeyChar();
+		for (int i = 0; i < cur; i++) {
+			if(current.charAt(i) == c) {
+				str1 = display.substring(0,i);
+				str2 = display.substring(i+1, cur);
+				display = str1 + c + str2 ;
+				
+			}
+			else if (current.charAt(i) != c && !used.contains(c) && !word.contains(""+c)) {
+				lives--;
+				used.add(c);
+				
+			}
+			l1.setText(display + "  " + "Lives Remaining: " + lives);
+		}
+	
 	}
 	
 }
